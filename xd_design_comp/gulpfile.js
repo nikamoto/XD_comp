94d5ed8362cd7a7e23f28f96662b1c7c798773a7
nikamoto <nikamoto.n6281@ae.auone-net.jp>
2021-06-21 22:42:53 +0900

    レスポンシブデザイン作成

--- xd_design_comp/gulpfile.js
+++ xd_design_comp/gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
