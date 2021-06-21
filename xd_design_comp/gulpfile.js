175a80a6f68d3533f66d521e1b000ecc952ba5a9
nikamoto <nikamoto.n6281@ae.auone-net.jp>
2021-06-21 17:37:32 +0900

    PostCSSのインストール

--- xd_design_comp/gulpfile.js
+++ xd_design_comp/gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
 });
@@ -13,3 +16,4 @@ gulp.task('sass', function() {
 gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', gulp.task('sass'));
 });
+
